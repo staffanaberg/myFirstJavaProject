ed3c074db6718b2c56e1045e63af52cfc31b452c
staff <staffan.aaberg@gmail.com>
2023-07-28 14:45:45 +0200

    Hello World

--- src/exercise11/HelloWorld.java
+++ src/exercise11/HelloWorld.java
@@ -3,8 +3,7 @@ package exercise11;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Dog!");
-        System.out.println("Hello Cat!");
-    }
+        System.out.println("Hello World!");
+        }
 
 }
