28e1a31a03c792c9bf645eb0c4e48bda255ed1fc
staff <staffan.aaberg@gmail.com>
2023-07-28 15:24:35 +0200

    More!!!!

--- src/exercise11/HelloWorld.java
+++ src/exercise11/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Cats!");
         System.out.println("Hello Dogs!");
-
+        System.out.println("Even more code");
         System.out.println("Hello World!! How are you!");
     }
 
