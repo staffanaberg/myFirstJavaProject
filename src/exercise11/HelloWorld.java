be3dab183571549e48da40dec58bf137409ab4ed
staff <staffan.aaberg@gmail.com>
2023-07-28 15:23:14 +0200

    Hello World!! How are you!

--- src/exercise11/HelloWorld.java
+++ src/exercise11/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
         System.out.println("Hello Cats!");
         System.out.println("Hello Dogs!");
 
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!! How are you!");
     }
 
 }
